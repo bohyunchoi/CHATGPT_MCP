022adcf9f82c946db9c87ace3a78cbaac9824aad
bohyunchoi <bohyun43@naver.com>
2025-05-27 16:40:57 +0900

    hello.py 업로드

--- GitHubPush.bat
+++ GitHubPush.bat
@@ -1,6 +1,12 @@
 @echo off
 cd /d %~dp0
 
+REM 원격 주소를 HTTPS로 재설정
+git remote set-url origin https://github.com/bohyunchoi/CHATGPT_MCP.git
+
+REM 인코딩 설정
+chcp 65001 >nul
+
 git add .
 git commit -m "hello.py 업로드"
 git push origin main
